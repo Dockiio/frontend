e5b26aebc9916a3eb3980b64dce674958f4bb889
dev-phantom <godsfavour1975@gmail.com>
2023-03-16 18:33:21 +0100

    feat: added a landing page

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,9 +1,23 @@
 import PreloaderImg from './img/preloaderImg.png';
 import Logo from './img/logo.png';
 import GeneralImg from './img/generalImg.png'
+import MidEllipse from "./img/MidEllipse.png"
+import MainImg from "./img/mainImg.png"
+import Avatar1 from './avatars/avatar1.png'
+import Avatar2 from './avatars/avatar2.png'
+import Avatar3 from './avatars/avatar3.png'
+import Avatar4 from './avatars/avatar4.png'
+import RobotAvatar from './avatars/robot.png'
 
 export {
     PreloaderImg,
     Logo,
-    GeneralImg
+    GeneralImg,
+    MidEllipse,
+    MainImg,
+    Avatar1,
+    Avatar2,
+    Avatar3,
+    Avatar4,
+    RobotAvatar,
 }
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,13 +1,94 @@
 import React from 'react'
-import { GeneralImg } from '../assets'
-export default function Landing() {
+import { 
+  GeneralImg, 
+  Logo, 
+  MidEllipse, 
+  MainImg,
+  Avatar1,
+  Avatar2,
+  Avatar3,
+  Avatar4,
+  RobotAvatar,
+} from '../assets'
+import { NavLink } from 'react-router-dom'
 
+export default function Landing() {
+const contributors = [
+  { handle: 'johndoe', avatarUrl: `${Avatar1}` },
+  { handle: 'janedoe', avatarUrl: `${Avatar2}` },
+  { handle: 'bobsmith', avatarUrl: `${Avatar3}` },
+  { handle: 'bobsmth', avatarUrl: `${Avatar4}` },
+];
   return (
     <div
-        className="w-screen object-cover h-screen bg-center bg-cover py-10 bg-no-repeat"
+        className="w-screen object-cover px-20 h-screen bg-center bg-cover bg-no-repeat"
         style={{ backgroundImage: `url(${GeneralImg})` }}
     >
-        
+      {/* decoration */}
+      <div className='fixed top-2 left-0 w-full pt-6 flex justify-center items-center'>
+        <img src={MidEllipse} alt="" className='w-[40%]'/>
+      </div>
+      {/* header */}
+      <div className='flex py-3 justify-between items-center'>
+        <div>
+          <img src={Logo} alt="logo" className='w-20 '/>
+        </div>
+        <div>
+          <NavLink 
+            to="/"
+            className="font-semibold font-poppins text-black text-lg underline"
+          >
+            Login
+          </NavLink>
+        </div>
+      </div>
+      {/* body */}
+      <div className='flex h-[80vh] items-center'>
+        <div className='z-[3]'>
+          <div>
+            <h1 className='text-7xl font-tomorrow font-bold w-[32rem] leading-[5rem] text-[#1A0634]'>Your Health Is In Our Care </h1>
+          </div>
+          <div className='w-[20rem] '>
+            <p className='leading-[30px] font-normal py-4 text-lg text-[#302F5C]'>Your health comes before anything, 
+              and always ready to provide solution to
+              your health problem.
+            </p>
+          </div>
+          <div>
+            <NavLink 
+              to="/register"
+            >
+              <button className='bg-gradient-to-br from-orange-400 to-yellow-200 font-tomorrow font-bold shadow-xl w-40 py-4 rounded-full'>Get Started</button>
+            </NavLink>
+          </div>
+        </div>
+        <div>
+          <img src={MainImg} alt=""  className='pb-2'/>
+        </div>
+        <div className='z-[3]'>
+          <div className="pl-8 py-4 flex -space-x-2 overflow-hidden">
+            {contributors.map((user, index) => (
+              <img 
+              key={user.handle} 
+              className="inline-block h-12 w-12 rounded-full ring-4 transition-transform transform hover:scale-110 ring-white" 
+              src={user.avatarUrl}
+              alt={user.handle}
+              style={{ animationDelay: `${index * 100}ms` }}
+              />
+            ))}
+          </div>
+          <div className="flex gap-3 mt-32 bg-white pl-3 items-center w-[16rem] rounded-lg py-2">
+            <div>
+              <img src={RobotAvatar} alt="" />
+            </div>
+
+            <div className='font-poppins font-bold text-md text-[#1A0634]'>
+              Chat With <br /> Dockii today
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
     
   )

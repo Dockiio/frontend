e9ce7425f87a376e67f17ca531cc282addb43ab5
dev-phantom <godsfavour1975@gmail.com>
2023-03-28 05:31:42 +0100

    changed robot image

--- src/components/common/topNav.jsx
+++ src/components/common/topNav.jsx
@@ -4,7 +4,7 @@ export default function TopNav() {
   return (
     <div className='w-full py-4 flex items-center gap-5 px-4 bg-white '>
         <div>
-            <img src={RobotAvatar} alt="" />
+            <img src="https://res.cloudinary.com/phantom1245/image/upload/v1679977841/dockii/Ellipse_13_y0fxfk.png" alt="" />
         </div>  
         <div className=''>
             <h1 className='text-lg font-tomorrow font-bold text-[#1A0634]'>DOCKII</h1>

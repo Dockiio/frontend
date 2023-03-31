91e512f744dcf7abf55a10462b9c224c6c7cb10e
dev-phantom <godsfavour1975@gmail.com>
2023-03-31 16:02:53 +0100

    completed the entire app

--- src/pages/chatBot.jsx
+++ src/pages/chatBot.jsx
@@ -151,15 +151,48 @@ import {
   { id: 132, name: 'prognosis' },
   { id: 133, name: 'itching' },
   ]
+const topics = ['itching', 'skin_rash', 'nodal_skin_eruptions', 'continuous_sneezing', 'shivering', 'chills', 'joint_pain', 'stomach_pain', 'acidity', 'ulcers_on_tongue', 'muscle_wasting', 'vomiting', 'burning_micturition', 'spotting_urination', 'fatigue', 'weight_gain', 'anxiety', 'cold_hands_and_feets', 'mood_swings', 'weight_loss', 'restlessness', 'lethargy', 'patches_in_throat', 'irregular_sugar_level', 'cough', 'high_fever', 'sunken_eyes', 'breathlessness', 'sweating', 'dehydration', 'indigestion', 'headache', 'yellowish_skin', 'dark_urine', 'nausea', 'loss_of_appetite', 'pain_behind_the_eyes', 'back_pain', 'constipation', 'abdominal_pain', 'diarrhoea', 'mild_fever', 'yellow_urine', 'yellowing_of_eyes', 'acute_liver_failure', 'fluid_overload', 'swelling_of_stomach', 'swelled_lymph_nodes', 'malaise', 'blurred_and_distorted_vision', 'phlegm', 'throat_irritation', 'redness_of_eyes', 'sinus_pressure', 'runny_nose', 'congestion', 'chest_pain', 'weakness_in_limbs', 'fast_heart_rate', 'pain_during_bowel_movements', 'pain_in_anal_region', 'bloody_stool', 'irritation_in_anus', 'neck_pain', 'dizziness', 'cramps', 'bruising', 'obesity', 'swollen_legs', 'swollen_blood_vessels', 'puffy_face_and_eyes', 'enlarged_thyroid', 'brittle_nails', 'swollen_extremeties', 'excessive_hunger', 'extra_marital_contacts', 'drying_and_tingling_lips', 'slurred_speech', 'knee_pain', 'hip_joint_pain', 'muscle_weakness', 'stiff_neck', 'swelling_joints', 'movement_stiffness', 'spinning_movements', 'loss_of_balance', 'unsteadiness', 'weakness_of_one_body_side', 'loss_of_smell', 'bladder_discomfort', 'foul_smell_ofurine', 'continuous_feel_of_urine', 'passage_of_gases', 'internal_itching', 'toxic_look_(typhos)', 'depression', 'irritability', 'muscle_pain', 'altered_sensorium', 'red_spots_over_body', 'belly_pain', 'abnormal_menstruation', 'dischromic_patches', 'watering_from_eyes', 'increased_appetite', 'polyuria', 'family_history', 'mucoid_sputum', 'rusty_sputum', 'lack_of_concentration', 'visual_disturbances', 'receiving_blood_transfusion', 'receiving_unsterile_injections', 'coma', 'stomach_bleeding', 'distention_of_abdomen',
+ 'history_of_alcohol_consumption', 'blood_in_sputum', 'prominent_veins_on_calf', 'palpitations', 'painful_walking', 'pus_filled_pimples', 'blackheads', 'scurring', 'skin_peeling', 'silver_like_dusting', 'small_dents_in_nails', 'inflammatory_nails', 'blister', 'red_sore_around_nose', 'yellow_crust_ooze', 'prognosis']
+
 export default function ChatBot() {
 
-  const [predict, setPredict] = useState("")
-  const [selected, setSelected] = useState(categories[0])
-  const [query, setQuery] = useState('')
   const [diagonsis, setDiagnosis] = useState("")
- const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const [filter, setFilter] = useState('');
+  const [selectedTopics, setSelectedTopics] = useState([]);
 
-  // Listen to changes in the messages array
+  const filteredTopics = topics.filter(
+    topic => !selectedTopics.includes(topic) && topic.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  const handleFilterChange = event => {
+    setFilter(event.target.value);
+  };
+
+  const handleTopicClick = topic => {
+    setSelectedTopics([...selectedTopics, topic]);
+    setFilter('');
+  };
+
+  const handleRemoveTopic = topic => {
+    setSelectedTopics(selectedTopics.filter(t => t !== topic));
+  };
+const handleInputKeyDown = event => {
+  if (event.key === ' ') {
+    event.preventDefault();
+    setSelectedTopics([...selectedTopics, filter.trim()]);
+    setFilter('');
+  } else if (event.key === 'Backspace') {
+    if (filter === '' && selectedTopics.length > 0) {
+      event.preventDefault();
+      setSelectedTopics(selectedTopics.slice(0, -1));
+    } else if (filter.endsWith(' ')) {
+      event.preventDefault();
+      setFilter(filter.slice(0, -1));
+    }
+  }
+};
+
 
         const fetchItems = async () => {
 
@@ -178,25 +211,18 @@ export default function ChatBot() {
 
         fetchItems();
 
-  const filteredCategory =
-    query === ''
-      ? categories
-      : categories.filter((category) =>
-          category.name
-            .toLowerCase()
-            .replace(/\s+/g, '')
-            .includes(query.toLowerCase().replace(/\s+/g, ''))
-        )
-
 const baseUrl = 'https://quixotic-earth-production.up.railway.app';
+const url = selectedTopics.reduce((acc, item, index) => {
+  const separator = index === 0 ? '?' : '&';
+  return `${acc}${separator}symp_lst=${item}`;
+}, `${baseUrl}/predict`);
 
 const predictDisease = async (e) => {
   e.preventDefault();
 
   try {
-    const params = selected.name.toString();
-    console.log(params)
-    const response = await axios.get(`${baseUrl}/predict?symp_lst=${params}&symp_lst=loss_of_smell`, {
+
+    const response = await axios.get(url, {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
@@ -209,28 +235,20 @@ const predictDisease = async (e) => {
     const userDocRef = doc(db, 'users', uid);
     await updateDoc(userDocRef, {
       messages: arrayUnion({
-        symptoms: params,
+        symptoms: selectedTopics,
         diagnosis: response.data.pred,
         timestamp: new Date().getTime(),
       }),
     });
-    // const userData = [{
-    //   symtoms: message,
-    //   diagnosis: response.data.pred,
-    //   timestamp: Date.now(),
-    // }];
-    // updateDoc(userDocRef, { messages: arrayUnion(...userData)})
+
+    setSelectedTopics([])
     return response.data;
   } catch (error) {
     console.error(error);
   }
+  
 };
 
-// // Example usage:
-// const symptoms = {
-//   symp_lst: ,
-// };
-// console.log(symptoms)
 
   return (
     <div
@@ -245,12 +263,12 @@ const predictDisease = async (e) => {
         <section className=' sticky top-0 left-0'>
           <SideNav />
         </section>
-        <main className='w-full'>
+        <main className='w-full z-[999]'>
           <section className='w-full fixed top-0 '>
             <TopNav />
           </section>
           <section className='mt-24 w-full '>
-<div className="flex flex-col mb-14  ">
+<div className="flex flex-col mb-32  ">
   <div className="flex-1 p-6">
     <div className="flex flex-col  space-y-4 w-full">
       <div className="rounded-lg flex gap-3 bg-white bg-opacity-30 p-4 max-w-[25rem] shadow-md">
@@ -261,16 +279,27 @@ const predictDisease = async (e) => {
           <p className="text-black">Hello , i am your AI Doctor what symptoms are you having? </p>
         </div>
       </div>
+      <div className="rounded-lg flex gap-3 bg-white bg-opacity-30 p-4 max-w-[30rem] shadow-md">
+        <div>
+            <img src="https://res.cloudinary.com/phantom1245/image/upload/v1679977841/dockii/Ellipse_13_y0fxfk.png" alt="" />
+        </div> 
+        <div>
+          <p className="text-black">Please provide me with enough details so i can accurately predict a diagnosis that may help. 
+          <br /> 
+          please note that this prediction might note be accurate because this is still a test phase. 
+          please separate your symptoms by comma(,) </p>
+        </div>
+      </div>
       <div className="flex-grow w-full">
         {messages &&
           messages.map((message) => (
             <div key={message.timestamp} className="flex flex-col w-full justify-between  items-center">
               <div className='w-full flex justify-end items-end'>
                 <div  className="rounded-lg bg-opacity-30 flex justify-center items-center gap-3 max-w-[30rem] bg-white p-4 shadow-md">
-                  <div>
-                    <img src="https://res.cloudinary.com/phantom1245/image/upload/v1680124827/dockii/Ellipse_15_v7m1zr.png" alt="" />
+                  <div className='w-[7rem]'>
+                    <img className="w-full h-auto " src="https://res.cloudinary.com/phantom1245/image/upload/v1680124827/dockii/Ellipse_15_v7m1zr.png" alt="" />
                   </div> 
-                  <div className=" ">{message.symptoms}</div>
+                  <div className="flex flex-wrap ">Dockii here are some of my symptoms {message.symptoms.map(symptom => symptom.replace(/_/g, ' ')).join(', ')}</div>
                 </div>
                 
               </div>
@@ -295,83 +324,66 @@ const predictDisease = async (e) => {
 
     </div>
   </div>
-
-</div>
-  <div className="px-8 fixed bg-white bg-opacity-20 bottom-0 w-full shadow-md">
-    <form className="flex w-full flex-row items-center gap-3 ">
-
-        <div className=" w-[60%]">
-          <Combobox value={selected} onChange={setSelected}>
-            <div className=" my-2">
-              <div className="w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-                <Combobox.Input
-                  className="w-full border-none outline-none py-6 pl-5 text-sm leading-5 text-gray-900 focus:ring-0"
-                  displayValue={(category) => category.name}
-                  onChange={(event) => setQuery(event.target.value)}
-                />
-                <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-                  <ChevronUpDownIcon
-                    className="h-5 w-5 text-gray-400"
-                    aria-hidden="true"
-                  />
-                </Combobox.Button>
-              </div>
-              <Transition
-                as={Fragment}
-                leave="transition ease-in duration-100"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-                afterLeave={() => setQuery('')}
+    <div className="flex fixed bg-red-200 bg-opacity-70 bottom-0 w-full shadow-md z-[999] flex-col gap-4">
+      <div className="flex px-7 pb-5 relative top-8 flex-row w-full overflow-x-auto justify-start gap-4">
+        {filteredTopics.map(topic => (
+          <div
+            key={topic}
+            className="px-3 py-1 w-max rounded-md bg-gray-100 text-gray-800 hover:bg-gray-200 cursor-pointer"
+            onClick={() => handleTopicClick(topic)}
+          >
+            {topic}
+          </div>
+        ))}
+      </div>
+      <div className="flex gap-4 mb-4">
+        <label htmlFor="filter" className="sr-only">
+          Filter topics
+        </label>
+        <div className="flex flex-col px-6 w-full flex-wrap">
+          <div className='flex  pl-4 flex-row gap-3'>
+            {selectedTopics.map(topic => (
+              <div
+                key={topic}
+                className="px-2 py-1 my-1 w-max rounded-md bg-blue-500 text-white flex items-center gap-2"
               >
-                <Combobox.Options className="fixed top-[22rem] max-h-60 w-[60%] overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                  {filteredCategory.length === 0 && query !== '' ? (
-                    <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
-                      Nothing found.
-                    </div>
-                  ) : (
-                    filteredCategory.map((category) => (
-                      <Combobox.Option
-                        key={category.id}
-                        className={({ active }) =>
-                          `flex cursor-default select-none py-3 rounded-md pl-10 pr-4 ${
-                            active ? 'bg-red-500 text-white' : 'text-gray-900'
-                          }`
-                        }
-                        value={category}
-                      >
-                        {({ selected, active }) => (
-                          <>
-                            <span
-                              className={`flex flex-row truncate ${
-                                selected ? 'font-medium' : 'font-normal'
-                              }`}
-                            >
-                              {category.name}
-                            </span>
-                            {selected ? (
-                              <span
-                                className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                                  active ? 'text-white' : 'text-primary'
-                                }`}
-                              >
-                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                              </span>
-                            ) : null}
-                          </>
-                        )}
-                      </Combobox.Option>
-                    ))
-                  )}
-                </Combobox.Options>
-              </Transition>
-            </div>
-          </Combobox>
+                <span>{topic}</span>
+                <button
+                  type="button"
+                  className="text-sm font-medium text-white hover:underline focus:outline-none"
+                  onClick={() => handleRemoveTopic(topic)}
+                >
+                  &times;
+                </button>
+              </div>
+            ))}
+          </div>
+          <div className='flex w-full gap-3 flex-row justify-center items-center'>
+          <div className='w-[70%]'>
+            <input
+              type="text"
+              id="filter"
+              name="filter"
+              value={selectedTopics.map(symptom => symptom.replace(/_/g, ' ')).join(', ') || filter}
+              onChange={handleFilterChange}
+              onKeyDown={handleInputKeyDown}
+              
+              className="flex-1 w-full pl-5 py-5 rounded-md bg-gray-100 text-gray-800 focus:outline-none "
+            />
+          </div>
+          
+          <div className='w-[30%]'>
+            <button type="submit" onClick={predictDisease} className="bg-red-900 px-10 text-white py-4 rounded-2xl">send</button>
+          </div>
+          </div>
+          
         </div>
-        <div className='w-[30%]'>
-          <button type="submit" onClick={predictDisease} className="bg-red-900 px-10 text-white py-4 rounded-2xl">send</button>
-        </div>
-    </form>
-  </div>
+        
+      </div>
+        
+    </div>
+</div>
+
           </section>
         </main>
         
